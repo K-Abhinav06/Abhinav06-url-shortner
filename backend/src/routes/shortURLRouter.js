99acbdfc21acf650ac83baddd63a46253a8510cd
K-Abhinav06 <khambhampatiabhinav@gmail.com>
2025-09-19 15:34:53 +0530

    mm

--- backend/src/routes/shortURLRouter.js
+++ backend/src/routes/shortURLRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createShortUrl, getLongUrl} from "../controllers/shortURLController.js";   
+import { createShortUrl, getLongUrl} from "./controllers/shortURLController.js";   
 import { isLoggedIn } from "../middlewares/authMiddleware.js";
 
 const shortURLRouter = Router();
